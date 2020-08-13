4b2bcaababe50cb547a7156a27a5fd620ca79fed
ahmadsufyan455 <ahmadsufyan455@gmail.com>
2020-08-13 20:59:54 +0700

    Move Token

--- app/src/main/java/com/fynzero/finalsubmission/network/ApiEndpoint.kt
+++ app/src/main/java/com/fynzero/finalsubmission/network/ApiEndpoint.kt
@@ -1,5 +1,6 @@
 package com.fynzero.finalsubmission.network
 
+import com.fynzero.finalsubmission.BuildConfig
 import com.fynzero.finalsubmission.model.User
 import com.fynzero.finalsubmission.model.UserResponse
 import retrofit2.Call
@@ -10,18 +11,18 @@ import retrofit2.http.Query
 
 interface ApiEndpoint {
     @GET("search/users")
-    @Headers("Authorization: token 3541a6a941ab3208817df38e63a1921ef5cf6452")
+    @Headers("Authorization: token ${BuildConfig.TOKEN}")
     fun getUser(@Query("q") username: String): Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 3541a6a941ab3208817df38e63a1921ef5cf6452")
+    @Headers("Authorization: token ${BuildConfig.TOKEN}")
     fun getDetail(@Path("username") username: String): Call<User>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 3541a6a941ab3208817df38e63a1921ef5cf6452")
+    @Headers("Authorization: token ${BuildConfig.TOKEN}")
     fun getFollowing(@Path("username") username: String): Call<ArrayList<User>>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 3541a6a941ab3208817df38e63a1921ef5cf6452")
+    @Headers("Authorization: token ${BuildConfig.TOKEN}")
     fun getFollower(@Path("username") username: String): Call<ArrayList<User>>
 }
